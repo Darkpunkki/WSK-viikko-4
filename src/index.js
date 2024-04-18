11f69d4515d083c7180ac89911cd7089a7ab6f50
Darkpunkki <jimi.pettila@metropolia.fi>
2024-04-18 10:15:49 +0300

    m

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import app from './app.js';
+import https from 'https';
+import fs from 'fs';
+// ************** sertifkaattien ja https serverin lisäys
+if (process.env.NODE_ENV === 'production') {
+  const sslkey = fs.readFileSync('/etc/pki/tls/private/ca.key');
+  const sslcert = fs.readFileSync('/etc/pki/tls/certs/ca.crt');
+
+  const options = {
+    key: sslkey,
+    cert: sslcert,
+  };
+  const httpsPort = process.env.HTTPS_PORT || 8000;
+  https.createServer(options, app).listen(httpsPort);
+}
 
 const hostname = '127.0.0.1';
 const port = 3000;
