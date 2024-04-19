379948cee7124d01f69487dea8b0a6a4bf0d7c7c
Darkpunkki <jimi.pettila@metropolia.fi>
2024-04-19 16:12:11 +0300

    customer backend logic

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import catRouter from './routes/cat-router.js';
 import userRouter from './routes/user-router.js';
 import tableRouter from './routes/table-router.js';
+import customerRouter from './routes/customer-router.js';
 import dotenv from 'dotenv';
 
 const router = express.Router();
@@ -12,5 +13,6 @@ router.get('/', (req, res) => {
 router.use('/cats', catRouter);
 router.use('/users', userRouter);
 router.use('/tables', tableRouter);
+router.use('/customers', customerRouter);
 
 export default router;
